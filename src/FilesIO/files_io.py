092055017f9f2476ea4f4fc3a733a728f72dbae1
Grokir <grokir13@yandex.ru>
2025-11-12 23:53:57 +0300

    Добавлены методы чтения и записи ключей

---
+++ src/FilesIO/files_io.py
@@ -0,0 +1,75 @@
+from Signature.Okamoto import SecretKey, PublicKey
+
+
+def save_keypair    (sk: SecretKey, pk: PublicKey, path_to_dir:str):
+  with open(path_to_dir+"okamoto_key", 'w') as sk_file:
+    sk_file.write(f"{sk.A()}\n{sk.B()}\n{sk.At()}\n{sk.Bt()}")
+
+  with open(path_to_dir+"okamoto_key.pub", 'w') as pk_file:
+    pk_file.write(f"{pk.U()}\n{pk.Ut()}")
+
+
+def load_keypair    (path_to_dir:str) -> tuple[SecretKey, PublicKey]:
+  sk_list: list[str] = []
+  pk_list: list[str] = []
+
+  with open(path_to_dir+"okamoto_key", 'r') as sk_file:
+    sk_list = sk_file.readlines()
+
+  with open(path_to_dir+"okamoto_key.pub", 'r') as pk_file:
+    pk_list = pk_file.readlines()
+
+  sk: SecretKey = SecretKey(
+    a   = int(sk_list[0]),
+    b   = int(sk_list[1]),
+    a_t = int(sk_list[2]),
+    b_t = int(sk_list[3])
+  )
+
+  pk: PublicKey = PublicKey(
+    u   = int(pk_list[0]),
+    u_t = int(pk_list[2])
+  )
+
+  return sk, pk
+
+
+def load_private_key(path_to_dir:str) -> SecretKey:
+  sk_list: list[str] = []
+
+  with open(path_to_dir+"okamoto_key", 'r') as sk_file:
+    sk_list = sk_file.readlines()
+
+  sk: SecretKey = SecretKey(
+    a   = int(sk_list[0]),
+    b   = int(sk_list[1]),
+    a_t = int(sk_list[2]),
+    b_t = int(sk_list[3])
+  )
+
+  return sk
+
+
+def load_public_key (path_to_dir:str) -> PublicKey:
+  pk_list: list[str] = []
+
+  with open(path_to_dir+"okamoto_key.pub", 'r') as pk_file:
+    pk_list = pk_file.readlines()
+
+  pk: PublicKey = PublicKey(
+    u   = int(pk_list[0]),
+    u_t = int(pk_list[2])
+  )
+
+  return pk
+
+
+
+
+
+def save_signature():
+  return
+
+
+def load_signature():
+  return
