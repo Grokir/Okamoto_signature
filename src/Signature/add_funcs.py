e12ff90c8f2cbdcc0ca4c9ad60990785bb3e498f
Grokir <grokir13@yandex.ru>
2025-11-13 00:45:58 +0300

    FIX метод блочного чтения файла: исправлен размер блока на 1024 символа (байта)

--- src/Signature/add_funcs.py
+++ src/Signature/add_funcs.py
@@ -35,7 +35,7 @@ def block_read_file(path_to_file: str) -> str:
   data:     bytes   = bytes()
   with open(path_to_file, 'rb', encoding="utf-8") as f:
     while True:
-      data = f.read(3)
+      data = f.read(1024)
       if not data:
         break
       res_data += sha3_512_hash(data)
