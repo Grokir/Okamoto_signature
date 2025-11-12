39088a00d14e196bf76899e3747335bb44541a99
Grokir <grokir13@yandex.ru>
2025-11-13 00:47:01 +0300

    Начало создания мэйна

--- src/main.py
+++ src/main.py
@@ -1,19 +1,12 @@
-from functions import *
+from sys import argv
+
+
+# project modules
 from Config import config
 
 
 def main():
-  # print("Hello world!")
-  # hash_str = sha3_256_hash("Hello world!".encode("utf-8"))
-  # print(f"Hash = {hash_str}")
-  # print(hex_to_int(hash_str))
-  # print(int("ffff", 16))
-
-
-  config.read("./config.yml")
-
-
-
+  return
 
 
 
