54257025e765bf4b88569f38802e1aeeb1867dcd
Grokir <grokir13@yandex.ru>
2025-11-21 19:47:43 +0300

    Добавлен __init__ для FilesIO

---
+++ src/FilesIO/__init__.py
@@ -0,0 +1,8 @@
+from .files_io   import save_keypair
+from .files_io   import load_keypair
+
+from .files_io   import load_public_key
+from .files_io   import load_private_key
+
+from .files_io   import save_signature
+from .files_io   import load_signature
