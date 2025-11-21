284e027072131b2b8be95327740fc04ac1cd730e
Grokir <grokir13@yandex.ru>
2025-11-21 19:58:09 +0300

    Добавлен класс подписи, изменена структура ключей и логика верификации

--- src/Signature/Okamoto.py
+++ src/Signature/Okamoto.py
@@ -3,31 +3,39 @@ from math           import log2, ceil
 from .add_funcs     import sha3_256_hash, hex_to_int
 
 class SecretKey:
-  def __init__(self, a:int, b:int, a_t:int, b_t:int):
+  def __init__(self, a:int, b:int):
     self.__a    = a
     self.__b    = b
-    self.__a_t  = a_t
-    self.__b_t  = b_t
 
   def A (self):
     return self.__a
   def B (self):
     return self.__b
-  def At(self):
-    return self.__a_t
-  def Bt(self):
-    return self.__b_t
-    
+
 
 class PublicKey:
-  def __init__(self, u:int, u_t:int):
+  def __init__(self, u:int):
     self.__u    = u
-    self.__u_t  = u_t
 
   def U (self):
     return self.__u
-  def Ut(self):
+    
+
+class Signature:
+  def __init__(self, a_z:int, b_z:int, u_t:int):
+    self.__a_z  = a_z
+    self.__b_z  = b_z
+    self.__u_t  = u_t
+
+  def Az (self):
+    return self.__a_z
+  
+  def Bz (self):
+    return self.__b_z
+  
+  def Ut (self):
     return self.__u_t
+
 
 
 
@@ -48,34 +56,40 @@ class OkamotoSign:
   def KeyGen(self) -> tuple[SecretKey, PublicKey]:
     bitlen: int = self.__get_bit_len(self.__q)
     a:      int = random.getrandbits(bitlen) % self.__q
-    b:      int = random.getrandbits(bitlen) % self.__q
-    a_t:    int = a
-    b_t:    int = b
+    b:      int = random.getrandbits(bitlen) % self.__q   
+    u:      int = ( pow(self.__g, a, self.__q) * pow(self.__h, b, self.__q) ) % self.__q
     
-    while (a_t == a) or (b_t == b):
-      if a_t == a:
-        a_t = random.getrandbits(bitlen) % self.__q
-      if b_t == b:
-        b_t = random.getrandbits(bitlen) % self.__q
-    
-    u:      int = ( pow(self.__g, a,   self.__q) * pow(self.__h, b,   self.__q) ) % self.__q
-    u_t:    int = ( pow(self.__g, a_t, self.__q) * pow(self.__h, b_t, self.__q) ) % self.__q
-
-    sk: SecretKey = SecretKey(a, b, a_t, b_t)
-    pk: PublicKey = PublicKey(u, u_t)
+    sk: SecretKey = SecretKey(a, b)
+    pk: PublicKey = PublicKey(u)
 
     return sk, pk
 
 
-  def Sign(self, message: str, sk:SecretKey) -> int:
-    c:   int = hex_to_int(sha3_256_hash(message.encode('utf-8')))
-    a_z: int = ( sk.At() + sk.A() * c )
-    b_z: int = ( sk.Bt() + sk.B() * c )
-    u_z: int = ( pow(self.__g, a_z, self.__q) * pow(self.__h, b_z, self.__q) ) % self.__q
+  def Sign(self, message: str, sk:SecretKey) -> Signature:
+    c:      int = hex_to_int(sha3_256_hash(message.encode('utf-8')))
+    bitlen: int = self.__get_bit_len(self.__q)
+    a_t:    int = sk.A()
+    b_t:    int = sk.B()
+    
+    while (a_t == sk.A()) or (b_t == sk.B()):
+      if a_t == sk.A():
+        a_t = random.getrandbits(bitlen) % self.__q
+      if b_t == sk.B():
+        b_t = random.getrandbits(bitlen) % self.__q
 
-    return u_z
+    a_z: int = ( a_t + sk.A() * c )
+    b_z: int = ( b_t + sk.B() * c )
+    u_t: int = ( pow(self.__g, a_t, self.__q) * pow(self.__h, b_t, self.__q) ) % self.__q
+
+    return Signature(a_z, b_z, u_t)
   
-  def Verify(self, message: str, signature: int, pk:PublicKey) -> bool:
+
+  def Verify(self, message: str, signature: Signature, pk:PublicKey) -> bool:
     c:   int = hex_to_int(sha3_256_hash(message.encode('utf-8')))
-    rhs: int = ( pk.Ut() * pow(pk.U(), c, self.__q) ) % self.__q
-    return (signature == rhs)
+    lhs: int = ( 
+      pow(self.__g, signature.Az(), self.__q) * 
+      pow(self.__h, signature.Bz(), self.__q) 
+    ) % self.__q
+
+    rhs: int = ( signature.Ut() * pow(pk.U(), c, self.__q) ) % self.__q
+    return (lhs == rhs)
